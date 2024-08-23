258a4d1254b6cf53053d8d8b5578fabb3efcdcb3
Rohit Pawar <rohitpawar5062@gmail.com>
2024-08-24 00:33:04 +0530

    fourth commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,27 @@
 const express=require('express');
 const User = require('../models/User');
+const { body,validationResult } = require('express-validator');
 const router = express.Router();
 
 
-router.post('/',(req,res)=>{
-    const user=User(req.body)
-    user.save()
-    console.log(req.body)
-    res.send("hello ")
+router.post('/', [body('name').isLength({min:5}),body('email').isEmail(),body('password').isLength({min:5})],(req,res)=>{
+   // const user=User(req.body)
+   // user.save()
+   try{
+    const result = validationResult(req);
+  if (result.isEmpty()) {
+    return User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    }).then(user=>res.json(user));
+  }
+
+  res.send({ errors: result.array() });
+}catch(error){
+    console.error(error.message);
+    res.status(500).send("some error occured")
+}
 })
 
 module.exports=router
